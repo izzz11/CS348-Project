6789faa777fb83dc87c9191cae569ea7a7c34106
youguxuanhe <x269li@uwaterloo.ca>
2025-06-19 12:06:34 -0400

    upload test-sample

--- backend/database/scripts/deletion.sql
+++
@@ -1,8 +0,0 @@
--- Clear all rows but keep table structure
-
-DELETE FROM playlist_songs;
-DELETE FROM listened;
-DELETE FROM playlists;
-DELETE FROM songs;
-DELETE FROM users;
-
--- backend/database/scripts/query.sql
+++
@@ -1,7 +0,0 @@
--- Show everything in every table
-
-SELECT * FROM users;
-SELECT * FROM songs;
-SELECT * FROM playlists;
-SELECT * FROM playlist_songs;
-SELECT * FROM listened;
